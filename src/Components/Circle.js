8e4bf2f9cb6b07d080e0dde49cfb8bab17bc9b61
Dilraj <144126612+Dilraj1602@users.noreply.github.com>
2025-01-24 02:13:11 +0530

    Delete src directory

--- src/Components/Circle.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import "../App.css";
-
-function Circle(){
-    return(
-        <div className="w-4 h-4 bg-[var(--first-color)] rounded-full "></div>
-    );
-}
-export default Circle;
