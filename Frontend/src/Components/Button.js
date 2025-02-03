a85fc9668afc6d3535a30faa0a7b07bb354b686c
Dilraj <144126612+Dilraj1602@users.noreply.github.com>
2025-02-03 05:38:42 +0530

    Update Button.js

--- Frontend/src/Components/Button.js
+++ Frontend/src/Components/Button.js
@@ -3,8 +3,8 @@ function Button({ text, icon, type = "button", onClick,isdarkmode }) {
       <button
           type={type}
           onClick={onClick}
-          className={`text-[15px] font-[var(--font-medium)] text-white hover:scale-[1.01] transition-all 
-          flex items-center justify-evenly hover:cursor-pointer py-[15px] px-[10px] border w-[150px] ${isdarkmode ? "border-[var(--dark-body-color)]" : "border-white"}  rounded-xl bg-[var(--first-color)]`}
+          className={`w-[95%] max-w-[150px] text-[15px] font-[var(--font-medium)] text-white hover:scale-[1.01] transition-all 
+          flex items-center justify-evenly hover:cursor-pointer py-[15px] px-[10px] border ${isdarkmode ? "border-[var(--dark-body-color)]" : "border-white"}  rounded-xl bg-[var(--first-color)]`}
       >
           {text}
           <i className={`${icon} text-white text-xl `}></i>
