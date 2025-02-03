8281b64c4adf5de77d9741728d1709f921da6b3f
Dilraj <144126612+Dilraj1602@users.noreply.github.com>
2025-02-03 05:41:36 +0530

    Update Skills.js

--- Frontend/src/Components/Skills.js
+++ Frontend/src/Components/Skills.js
@@ -14,28 +14,28 @@ function Skills({ isdarkmode }) {
   const { languages, frameworks, tools, languages1, img4, img1, img2, img3, rate1, rate2, rate3, rate4 } = user_data;
   // 
   return (
-    <div className={`md:pt-[5.5rem] flex flex-col relative justify-center items-center ${isdarkmode ? "text-white" : "text-black"}   `}>
+    <div className={`w-[90%] sm:w-full px-[5%] sm:px-0 md:pt-[5.5rem] flex flex-col relative justify-center items-center ${isdarkmode ? "text-white" : "text-black"}   `}>
       <h1 className={`text-[2rem] ${isdarkmode ? "text-white" : "text-black"} font-bold text-[var(--body-font)] font-Poppins`}>Skills</h1>
       <p className={`text-[1.1rem] ${isdarkmode ? "text-gray-400" : "text-gray-500"} mb-10`}>My Technical Skills</p>
 
-      <div className="grid grid-cols-1 gap-[40px] md:grid-cols-2 md:gap-[80px] mt-8 pl-[1%] md:pl-[7%] ">
+      <div className="grid grid-cols-1  gap-[40px] md:grid-cols-2 md:gap-[80px] mt-8 pl-[1%] md:pl-[7%] ">
         {/* Languages */}
         <div className="relative">
           <div>
             <PiBracketsAngleBold
-              size="2.5rem"
-              className="text-[var(--first-color)] hover:cursor-pointer absolute left-[-13%] sm:left-[-12%] top-[10px] "
+              className="text-[2rem] sm:text-[2.5rem] text-[var(--first-color)] hover:cursor-pointer sm:absolute left-[-13%] sm:left-[-12%] top-[10px]"
               onClick={() => setIsDown1(!isdown1)}
             />
+
           </div>
-          <div className={`font-Poppins  text-xl font-[600]`}>Languages</div>
+          <div className={`w-full font-Poppins  text-xl font-[600]`}>Languages</div>
           <p
             className="flex items-center hover:cursor-pointer text-gray-400 select-none "
             onClick={() => setIsDown1(!isdown1)}
           >
             Languages that I have picked up over the years
             <i
-              className={`uil ${isdown1 ? "uil-angle-down" : "uil-angle-up"} skills_arrow ml-2`}
+              className={`uil ${!isdown1 ? "uil-angle-down" : "uil-angle-up"} skills_arrow ml-2`}
             ></i>
           </p>
           {isdown1 && <Skills_section_1 languages1={languages1} img4={img4} rate4={rate4} />}
@@ -47,18 +47,18 @@ function Skills({ isdarkmode }) {
           <div>
             <PiBracketsCurlyBold
               size="2.5rem"
-              className="text-[var(--first-color)] hover:cursor-pointer absolute left-[-13%] sm:left-[-12%] top-[10px] "
+              className="text-[var(--first-color)] hover:cursor-pointer sm:absolute left-[-13%] sm:left-[-12%] top-[10px] "
               onClick={() => setIsDown2(!isdown2)}
             />
           </div>
           <div className="font-Poppins text-xl font-[600]">Libraries & Frameworks</div>
           <p
             className="flex items-center hover:cursor-pointer text-gray-400 select-none "
-            onClick={() => setIsDown2(!isdown2)} 
+            onClick={() => setIsDown2(!isdown2)}
           >
             Libraries and Frameworks that I prefer to work with
             <i
-              className={`uil ${isdown2 ? "uil-angle-down" : "uil-angle-up"} skills_arrow ml-2`}
+              className={`uil ${!isdown2 ? "uil-angle-down" : "uil-angle-up"} skills_arrow ml-2`}
             ></i>
           </p>
           {isdown2 && <Skills_section frameworks={frameworks} img2={img2} rate2={rate2} />}
@@ -69,7 +69,7 @@ function Skills({ isdarkmode }) {
           <div>
             <GoTools
               size="2.5rem"
-              className="text-[var(--first-color)] hover:cursor-pointer absolute left-[-13%] sm:left-[-12%] top-[10px] "
+              className="text-[var(--first-color)] hover:cursor-pointer sm:absolute left-[-13%] sm:left-[-12%] top-[10px] "
               onClick={() => setIsDown3(!isdown3)}
             />
           </div>
@@ -80,7 +80,7 @@ function Skills({ isdarkmode }) {
           >
             Tools that I know and use on a daily basis
             <i
-              className={`uil ${isdown3 ? "uil-angle-down" : "uil-angle-up"} skills_arrow ml-2`}
+              className={`uil ${!isdown3 ? "uil-angle-down" : "uil-angle-up"} skills_arrow ml-2`}
             ></i>
           </p>
 
