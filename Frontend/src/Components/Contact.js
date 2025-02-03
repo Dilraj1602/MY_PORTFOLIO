b953c42981a069d4831172795a5b087d51b5ab6f
Dilraj <144126612+Dilraj1602@users.noreply.github.com>
2025-02-03 05:39:00 +0530

    Update Contact.js

--- Frontend/src/Components/Contact.js
+++ Frontend/src/Components/Contact.js
@@ -7,11 +7,11 @@ function Contact({isdarkmode}) {
     console.log(contacts);
 
     return (
-        <div className={`pt-[5.5rem] flex flex-col  relative justify-between items-center w-full mb-10 lg:mb-0 ${isdarkmode ? "text-white" : "text-black"}`}>
-            <h1 className="text-[2rem] font-bold text-[var(--body-font)] font-Poppins">Contact Me</h1>
+        <div className={`pt-[5.5rem] flex flex-col  relative justify-between items-center px-[5%] sm:px-0 mb-10 lg:mb-0 ${isdarkmode ? "text-white" : "text-black"}`}>
+            <h1 className="text-[2rem]  font-bold text-[var(--body-font)] font-Poppins">Contact Me</h1>
             <p className={`text-[1.1rem] ${isdarkmode?"text-gray-400":"text-gray-500"} `}>Want to connect? My inbox is always open!</p>
 
-            <div className="flex flex-col sm:flex-row lg:pt-[7%] md:justify-between mt-12 sm:space-x-[7%] md:space-x-[16%] relative " >
+            <div className="flex flex-col sm:flex-row px-[5%] w-full lg:pt-[7%] justify-center sm:justify-between  mt-12 sm:space-x-[7%] md:space-x-[16%] relative " >
                 {/* Way to connect */}
                 <div className="flex flex-col space-y-[2%]  mb-[20px]  sm:space-y-[20%]  ">
                     {contacts.map((contact, index) => {
@@ -28,7 +28,7 @@ function Contact({isdarkmode}) {
                 </div>
 
                 {/* Form */}
-                <div className="w-[300px] sm:w-[460px] md:w-[460px] lg:w-[600px] ">
+                <div className="w-[95%] md:w-[460px] lg:w-[600px] ">
                     <Form isdarkmode={isdarkmode} />
                 </div>
             </div>
