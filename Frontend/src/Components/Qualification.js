c1c7d1357a9e0a338c078e5f59d4cd16f1bb1caf
Dilraj <144126612+Dilraj1602@users.noreply.github.com>
2025-02-03 05:51:32 +0530

    Update Qualification.js

--- Frontend/src/Components/Qualification.js
+++ Frontend/src/Components/Qualification.js
@@ -5,7 +5,7 @@ import data from "../data";
 import Q_section from "./Q_section";
 import Circle from "./Circle";
 
-function Qualification({ isdarkmode }) {
+function Qualification({isdarkmode}) {
     const [Field, setField] = useState("Education");
     const { education, place, location, year, work, w_place, w_year, w_location } = data;
     console.log(data);
@@ -15,44 +15,43 @@ function Qualification({ isdarkmode }) {
     }
 
     return (
-        <div className={`pt-[5.5rem] w-[90%] sm:w-full px-[5%] sm:px-0 flex flex-col relative justify-center items-center ${isdarkmode ? "text-white" : "text-black"} `}>
+        <div className={`pt-[5.5rem] flex flex-col relative justify-center items-center ${isdarkmode ?"text-white":"text-black"} `}>
             {/* Header */}
-            <h1 className="text-[1.5rem] sm:text-[2rem] font-bold text-[var(--body-font)] font-Poppins">
+            <h1 className="text-[2rem] font-bold text-[var(--body-font)] font-Poppins">
                 Qualification
             </h1>
-            <p className={`text-[1.1rem] ${isdarkmode ? "text-gray-400" : "text-gray-500"} `}>My Educational Qualification & Work Experience</p>
+            <p className={`text-[1.1rem] ${isdarkmode?"text-gray-400":"text-gray-500"} `}>My Educational Qualification & Work Experience</p>
 
             {/* Tabs */}
-            <div className="mt-10 w-[90%] sm:w-full px-[5%] sm:px-0 ">
-                <div className="sm:w-full flex sm:space-x-[50px] justify-center items-center">
+            <div className="mt-10 ">
+                <div className="flex space-x-[50px] justify-center items-center">
                     <div
-                        className={`w-[90%] sm:w-full px-[5%] sm:px-0 flex items-center space-x-2 cursor-pointer transition-colors duration-200 ${Field === "Education"
-                                ? "text-[var(--first-color-alt)]"
-                                : isdarkmode
-                                    ? "text-[var(--dark-text-color)]"
+                        className={`flex items-center space-x-2 cursor-pointer transition-colors duration-200 ${
+                            Field === "Education" 
+                                ? "text-[var(--first-color-alt)]" 
+                                : isdarkmode 
+                                    ? "text-[var(--dark-text-color)]" 
                                     : "text-[var(--title-color)]"
-                            }`}
-
+                        }`}
+                        
                         onClick={() => handleClick("Education")}
                     >
-                        <RiGraduationCapLine
-                            className="w-[1.5rem] sm:w-[1.8rem] h-[1.5rem] sm:h-[1.8rem]"
-                        />
-
-                        <h1 className="font-Poppins font-[500] text-[1.5rem]  sm:text-[1.8rem]">Education</h1>
+                        <RiGraduationCapLine style={{ width: '1.8rem', height: '1.8rem' }} />
+                        <h1 className="font-Poppins font-[500] text-[1.8rem]">Education</h1>
                     </div>
                     <div
-                        className={`w-[45%] flex items-center space-x-2 cursor-pointer transition-colors duration-200 ${Field === "Work"
-                                ? "text-[var(--first-color-alt)]"
-                                : isdarkmode
-                                    ? "text-[var(--dark-text-color)]"
+                        className={`flex items-center space-x-2 cursor-pointer transition-colors duration-200 ${
+                            Field === "Work" 
+                                ? "text-[var(--first-color-alt)]" 
+                                : isdarkmode 
+                                    ? "text-[var(--dark-text-color)]" 
                                     : "text-[var(--title-color)]"
-                            }`}
-
+                        }`}
+                        
                         onClick={() => handleClick("Work")}
                     >
-                        <MdWorkOutline className="w-[1.5rem] sm:w-[1.8rem] h-[1.5rem] sm:h-[1.8rem]" />
-                        <h1 className={`font-Poppins font-[500] text-[1.5rem]  sm:text-[1.8rem]`}>Work</h1>
+                        <MdWorkOutline style={{ width: '1.8rem', height: '1.8rem' }} />
+                        <h1 className={`font-Poppins font-[500] text-[1.8rem]  `}>Work</h1>
                     </div>
                 </div>
 
